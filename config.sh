77af4b8f5b2d7636dddb3087a13ea8ad08f65c78
Devobass <devobass@disroot.org>
2026-02-08 17:55:50 +0700

    added config file

---
+++ config.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+export DVBK_CONF_MC_PATH="/var/www/minecraft"
+export DVBK_CONF_NGINX_CONF_PATH="/etc/nginx/nginx.conf"
+export DVBK_CONF_CONDUIT_PATH="/etc/conduit/conduit.toml"
+export DVBK_CONF_CONDUIT_VAR_PATH="/var/lib/conduit/"
