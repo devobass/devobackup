061d53eb36752595226789154aa280b8591dd4b1
Devobass <devobass@disroot.org>
2026-02-10 12:25:36 +0700

    Reapply "new processing pipeline implemented" oopsies lmao
    This reverts commit 37d8c0b65998f5327e428ee1cb08644eb40f6cb7.

--- config.sh
+++ config.sh
@@ -2,5 +2,5 @@
 
 export DVBK_CONF_MC_PATH="/var/www/minecraft"
 export DVBK_CONF_NGINX_CONF_PATH="/etc/nginx/nginx.conf"
-export DVBK_CONF_CONDUIT_PATH="/etc/conduit/config.toml"
+export DVBK_CONF_CONDUIT_PATH="/etc/conduit/conduit.toml"
 export DVBK_CONF_CONDUIT_VAR_PATH="/var/lib/conduit/"
